d84c92ad75182696d230e3eb22bc5a1874048f38
Fred Lorry <gotoxa13@gmail.com>
2016-03-09 14:33:21 +0300

    Class-based template views.

--- source/JAFImageboard/app_userinterface/urls.py
+++ source/JAFImageboard/app_userinterface/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='homepage'),
-    url(r'^(?P<tpc>[a-z]{2,3})$', views.threadlist, name='threadlist'),
-    url(r'^(?P<tpc>[a-z]{2,3})/(?P<thr>\d{1,5})$', views.threadpage, name='threadpage'),
+    url(r'^$', views.HomepageView.as_view(), name='homepage'),
+    url(r'^(?P<tpc>[a-z]{2,3})$', views.ThreadlistView.as_view(), name='threadlist'),
+    url(r'^(?P<tpc>[a-z]{2,3})/(?P<thr>\d{1,5})$', views.ThreadpageView.as_view(), name='threadpage'),
     url(r'.+$', views.homepage_redirect, name='redirect')
 ]
--- source/JAFImageboard/app_userinterface/views.py
+++ source/JAFImageboard/app_userinterface/views.py
@@ -1,25 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic import View, TemplateView
 
 # Create your views here.
 
-def homepage(request):
-    return HttpResponse("Hello, world. You're at the homepage.\
-                    <br> It's supposed to be a list of available topics.\
-                    <br> Check out thread <a href='/rus'>rus</a>, for example.\
-                    <br> Or <a href='/vg'>vg</a>.\
-                    <br> Actually, any combination of 2 or 3 letters will do.\
-                    ")
+class HomepageView(TemplateView):
+    """Homepage view."""
+    template_name = "homepage.html"
 
-def threadlist(request, tpc):
-    return HttpResponse("This is list of threads for topic /" + tpc + "/.\
-                        <br> You can choose from those:\
-                        <br> <a href='/"+tpc+"/1'>1. First</a>\
-                        <br> <a href='/"+tpc+"/2'>2. Second</a>\
-                        <br> <a href='/"+tpc+"/3'>3. Third</a>" )
 
-def threadpage(request, tpc, thr):
-    return HttpResponse("This is thread page for thread №" + thr + " from topic /" + tpc + "/.")
+class ThreadlistView(TemplateView):
+    """Threadlist view."""
+    template_name = "threadlist.html"
+
+class ThreadpageView(TemplateView):
+    """Threadpage view."""
+    template_name = "threadpage.html"
 
 def homepage_redirect(request):
     return HttpResponseRedirect("/")
